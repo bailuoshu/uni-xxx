a523bd53a14033b872a4542791e036ad7311623c
chenyanjun <chenyanjun@asiainfo.com>
2022-08-17 16:18:15 +0800

    初始化

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+
+// vant 按需引入
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+});
